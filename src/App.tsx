2ae991e465261cf03722202e2a512968474071d6
Hady <hady_awayda@hotmail.com>
2023-03-26 02:19:37 +0300

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 type Subtasks = {
@@ -15,47 +13,62 @@ type Tasks = {
   status: string,
 }
 
-type Column = {
+type Columns = {
   name: string,
   tasks: Tasks[],
 }
 
 type Boards = {
   name: string,
-  columns: Column[],
+  columns: Columns[],
 }
 
 function App() {
-  const [boards, setBoards] = useState<Boards[]>();
+  const [boards, setBoards] = useState<Boards[]>([]);
   const [createBoards, setCreateBoards] = useState(false);
+  const [isSidebar, setIsSidebar ] = useState(true);
+
+  async function getData () {
+    const res = await fetch('/data.json');
+    const data = await res.json();
+    setBoards(data.boards);
+  }
+  useEffect(() => {
+    getData();
+  }, [])
+
+  console.log(boards);
+
 
   function handleCreateNewBoard (e : React.MouseEvent) {
     setCreateBoards(true)
   }
-  
+  function handleSidebar () {
+    setIsSidebar(!isSidebar)
+  }
+  console.log(isSidebar)
   return (
-    <div className="flex justify-between h-screen w-full background">
-      <div className='flex flex-col justify-between w-80 sm:w-80 border-r-2 border-gray-600 border-opacity-40'>
+    <div className="flex justify-between h-screen w-full">
+      <div className={`flex flex-col justify-between w-80 sm:w-80 border-r-2 border-gray-600 border-opacity-40 transition-all ${isSidebar ? 'sidebar-open' : ''}`}>
         <div className='flex flex-col'>
           <div className='flex flex-col mb-6 justify-between h-20 px-12'>
             <h1 className='pt-6 text-4xl text-white'>Kanban</h1>
           </div>
+          <div className='py-3 px-6 sm:px-12 spacing'>ALL BOARDS (8)</div>
           <div className='flex flex-col items-start pr-6'>
-            {/* {boards.map((a) => {
-              <div>{a}</div>
-            })} */}
-            <div className='py-3 px-6 sm:px-12 spacing'>ALL BOARDS (8)</div>
-            <button className='py-3 pl-6 sm:pl-12 text-left w-full rounded-r-full boards'>Platform Launch</button>
-            <button className='py-3 pl-6 sm:pl-12 buttons'>Marketing Plan</button>
-            <button className='py-3 pl-6 sm:pl-12 buttons'>Roadmap</button>
+            {boards.map((a) => (
+              <button className='buttons py-3 pl-6 sm:pl-12 text-left w-full rounded-r-full boards'>{a.name}</button>
+            ))}
+          </div>
+          <div>  
             <button onClick={handleCreateNewBoard} className='py-3 pl-6 sm:pl-12'>+ Create New Board</button>
           </div>
         </div>
         <div className='flex flex-col justify-between h-30'>
           <div className='h-12 toggle text-center mx-6 rounded-md py-3'>Toggle Dark Mode</div>
-          <button className='h-20 text text-center py-4'>Hide Sidebar</button>
+          <button onClick={handleSidebar} className='h-20 text text-center py-4'>Hide Sidebar</button>
         </div>
-      </div>
+      </div> 
       <div className="card">
         xxxxxxxxxxxxxxxxxxxxxxx
       </div>
